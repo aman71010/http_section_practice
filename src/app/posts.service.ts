ac85c7ae71a7129225178ed7797d297949487fe2
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-27 15:17:41 +0530

    uses options like headers, params, observe, responseType

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { catchError, map } from "rxjs/operators";
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
 
 import { Post } from "./post.model";
@@ -15,18 +15,30 @@ export class PostsService {
 
     createAndStorePost(postData: Post){
         this.http.post<{name: string}>(
-            'https://testing-e500b-default-rtdb.firebaseio.com/posts.json', postData).subscribe(
-                (responseData) => {
-                    console.log(responseData);
-                },
-                (error) => {
-                    this.error.next(error.message);
-                }
-            );
+            'https://testing-e500b-default-rtdb.firebaseio.com/posts.json', postData,
+            { 
+                observe: 'response'
+            }
+        ).subscribe(
+            (responseData) => {
+                console.log(responseData);
+            },
+            (error) => {
+                this.error.next(error.message);
+            }
+        );
     }
 
     fetchPosts(){
-        return this.http.get<{ [key: string]: Post }>('https://testing-e500b-default-rtdb.firebaseio.com/posts.json')
+        let searchParams = new HttpParams();
+        searchParams.append('print', 'pretty');
+        return this.http.get<{ [key: string]: Post }>('https://testing-e500b-default-rtdb.firebaseio.com/posts.json',
+        {
+            headers: new HttpHeaders({'customHeader': 'Hello'}),
+            params: searchParams,
+            responseType: 'json'
+        }
+        )
         .pipe(
             map(responseData => {
                 const postArray: Post[] = [];
@@ -45,6 +57,18 @@ export class PostsService {
     }
 
     deletePosts(){
-        return this.http.delete('https://testing-e500b-default-rtdb.firebaseio.com/posts.json');
+        return this.http.delete('https://testing-e500b-default-rtdb.firebaseio.com/posts.json',{
+            observe: 'events',
+            responseType: 'text'
+        }).pipe(
+            tap(event => {
+                if(event.type === HttpEventType.Sent){
+                    //.....
+                    console.log(event.type);
+                }
+                if(event.type === HttpEventType.Response)
+                    console.log(event.body);
+            }
+        ))
     }
 }
